92522e0e055cfb3acac75d33da581efaf9b1cb7e
Keilani K Burak <kb17180@srcs.loc>
2017-01-31 09:34:22 -0800

    3

--- calculator_tests.js
+++ calculator_tests.js
@@ -7,8 +7,30 @@ QUnit.test( "Add digits test", function( assert ) {
 
 // Test adding one, then two decimals
 QUnit.test( "Add decimal test", function( assert ) {
+    clearCurrent();
     addDecimal();
     addDigit('2');
     addDecimal();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+});
+
+QUnit.test("Factorial test", function( assert ) {
+    clearCurrent();
+    addDigit('4');
+    factorial();
+    assert.equal(document.getElementById("screen").value, "24", "Passed - Expected 24");
+});
+
+QUnit.test("Square test", function( assert ){
+    clearCurrent();
+    addDigit('6');
+    square();
+    assert.equal(document.getElementById("screen").value, "36", "Passed - Expected 36");
+});
+
+QUnit.test("Square root test", function( assert ){
+    clearCurrent();
+    addDigit('49');
+    squareRoot();
+    assert.equal(document.getElementById("screen").value, "7", "Passed - Expected 49");
 });
