531fe71498596d09627ead657479327ee65e94bb
SineadSwayne <ssinead3@gmail.com>
2017-02-02 09:24:50 -0800

    Calc Tests

--- calculator_tests.js
+++ calculator_tests.js
@@ -32,5 +32,47 @@ QUnit.test("Square root test", function( assert ){
     clearCurrent();
     addDigit('49');
     squareRoot();
-    assert.equal(document.getElementById("screen").value, "7", "Passed - Expected 49");
+    assert.equal(document.getElementById("screen").value, "7", "Passed - Expected 7");
+});
+
+QUnit.test( "Change sign test", function( assert ) {
+    clearCurrent();
+    addDigit('12');
+    changeSign();
+    assert.equal(document.getElementById("screen").value, "-12", "Passed - Expected -12");
+});
+
+QUnit.test( "Change percentage test", function( assert ) {
+    clearCurrent();
+    addDigit('60');
+    percentage();
+    assert.equal(document.getElementById("screen").value, "0.6", "Passed - Expected 0.6");
+});
+
+QUnit.test( "Change current test", function( assert ) {
+    clearCurrent();
+    addDigit('5');
+    storeOperator("+");
+    addDigit('3');
+    clearCurrent();
+    addDigit('7');
+    calculate();
+    assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
+    allClear();
+});
+
+QUnit.test( "Divide by zero test", function( assert ) {
+    clearCurrent();
+    addDigit('6');
+    storeOperator("/");
+    addDigit('0');
+    calculate();
+    assert.equal(document.getElementById("screen").value, "Error Divide By Zero", "Passed - Expected Error Divide By Zero");
+});
+
+QUnit.test("Square test", function( assert ){
+    clearCurrent();
+    addDigit('6');
+    square();
+    assert.equal(document.getElementById("screen").value, "36", "Passed - Expected 36");
 });
